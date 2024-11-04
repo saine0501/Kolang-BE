694d47b5426a187e8eaa61c2e918a09ac57e7e43
saine0501 <saine0501@gmail.com>
2024-11-04 20:55:25 +0900

    feat: db 테이블 변경 및 관계 설정

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, ForeignKey
+from sqlalchemy.orm import relationship
 from db.database import Base
 from datetime import datetime
 
@@ -8,31 +9,40 @@ from sqlalchemy import String, DateTime
 class User(Base):
     __tablename__ = "users"
     
-    userid = Column(String(255), primary_key=True, index=True)
+    user_id = Column(String(255), primary_key=True)
     email = Column(String(255), nullable=False)
     name = Column(String(255))
     created_at = Column(DateTime(6), nullable=False, default=datetime.utcnow)
     deleted_at = Column(DateTime(6), nullable=True)
     onboarding = Column(Boolean, nullable=False, default=False)
     onboarding_info = Column(JSON(String(255)), nullable=True)
+    
+    messages = relationship("Message", back_populates="user")
+    chats = relationship("ChatList", back_populates="user")
 
 # chatlist 테이블
 class ChatList(Base):
     __tablename__ = "chatlist"
     
-    chatid = Column(String(255), primary_key=True, index=True)
-    userid = Column(String(255), ForeignKey("users.userid"), nullable=False)
+    chat_id = Column(String(255), primary_key=True)
+    user_id = Column(String(255), ForeignKey("users.user_id"), nullable=False)
     summary = Column(String(255))
     category = Column(String(255))
     created_at = Column(DateTime(6), nullable=False, default=datetime.utcnow)
+    
+    user = relationship("User", back_populates="chats")
+    messages = relationship("Message", back_populates="chat")
 
 # messages 테이블
 class Message(Base):
     __tablename__ = "messages"
     
-    message_id = Column(Integer, primary_key=True, index=True)
-    userid = Column(String(255), ForeignKey("users.userid"), nullable=False)
-    chatid = Column(String(255), ForeignKey("chatlist.chatid"), nullable=False)
+    message_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(String(255), ForeignKey("users.user_id"), nullable=False)
+    chat_id = Column(String(255), ForeignKey("chatlist.chat_id"), nullable=False)
     created_at = Column(DateTime(6), nullable=False, default=datetime.utcnow)
     message = Column(String(255))
-    answer = Column(String(255))
+    is_answer = Column(Boolean, nullable=False, default=False)
+    
+    user = relationship("User", back_populates="messages")
+    chat = relationship("ChatList", back_populates="messages")

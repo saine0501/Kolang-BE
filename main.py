35de9cd4deffbb7f15819a8a468e492eac597396
saine0501 <saine0501@gmail.com>
2024-11-11 23:40:37 +0900

    feat: 세션 미들웨어 추가

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Depends, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from sqlalchemy.orm import Session
 from db.database import engine, get_db
 from db import models, schemas
 
-from routes import chat, chatlist, stt
+from routes import chat, chatlist, stt, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -24,6 +25,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="YApzHZpysT2E7UQNXdaGPAm44seOQcnF",
+    session_cookie="google-login",
+    same_site="lax",
+    https_only=False
+)
+
 @app.get("/")
 async def read_root():
     return {"Hello" : "World"}
@@ -32,3 +41,4 @@ app.include_router(router)
 app.include_router(chatlist.router)
 app.include_router(stt.router)
 app.include_router(chat.router)
+app.include_router(auth.router)

77e380f3883326fe10deccc98d6bc86d1aacc9c2
saine0501 <saine0501@gmail.com>
2024-12-03 23:08:51 +0900

    refactor: chatlist 조회 API router 경로 구조 변경

--- routes/chatlist.py
+++ routes/chatlist.py
@@ -8,18 +8,18 @@ from routes.auth import get_current_user
 from db import crud
 
 router = APIRouter(
-    prefix="/api",
+    prefix="/api/chatlist",
     tags=["Chatlist"]
 )
 
-@router.get("/chatlist", response_model=List[ChatListResponse], description="채팅방 내역 조회")
+@router.get("/", response_model=List[ChatListResponse], description="채팅방 내역 조회")
 async def get_user_chats(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
     return crud.get_user_chats(db=db, user_id=current_user.user_id)
 
-@router.get("/chatlist/detail/{chatId}", response_model=ChatDetailResponse)
+@router.get("/detail/{chatId}", response_model=ChatDetailResponse)
 async def get_chat_messages(
     chatId: str,
     db: Session = Depends(get_db),
